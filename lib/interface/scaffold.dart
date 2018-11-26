e845ecda054a350566940e98cdef0ffd70ef9f68
serdarde <serdar@7style.net>
2018-11-26 06:13:29 +0100

    placeholder image

--- lib/interface/scaffold.dart
+++ lib/interface/scaffold.dart
@@ -26,159 +26,167 @@ class ScaffoldExample extends StatelessWidget {
         ],
       ),
       body: new Center(
-        // child: new Text('Merhaba Scaffold', textDirection: TextDirection.ltr),
-        child: new GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this would produce 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 Widgets that display their index in the List
-          children: <Widget>[
-            new GestureDetector(
-              onTap: () => Navigator.pushNamed(context, "/firstPage"),
-              child: Container(
-                decoration: BoxDecoration(
+          // child: new Text('Merhaba Scaffold', textDirection: TextDirection.ltr),
+          child: new GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this would produce 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 Widgets that display their index in the List
+        children: <Widget>[
+          new GestureDetector(
+            onTap: () => Navigator.pushNamed(context, "/firstPage"),
+            child: Container(
+              decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: NetworkImage('https://scontent-frt3-2.cdninstagram.com/vp/1fa729d72a7f671b79166aa2c4343ee2/5C8D12DA/t51.2885-15/fr/e15/s1080x1080/45835041_619192898498236_5011079989747444985_n.jpg'),
-                    fit: BoxFit.cover,
-                  )
-                ),
-                margin: EdgeInsets.all(10.0),
+                image: NetworkImage(
+                    'https://scontent-frt3-2.cdninstagram.com/vp/1fa729d72a7f671b79166aa2c4343ee2/5C8D12DA/t51.2885-15/fr/e15/s1080x1080/45835041_619192898498236_5011079989747444985_n.jpg'),
+                fit: BoxFit.cover,
+              )),
+              margin: EdgeInsets.all(10.0),
+              alignment: Alignment.bottomCenter,
+              child: Transform(
                 alignment: Alignment.bottomCenter,
-                child: Transform(
-                  alignment: Alignment.bottomCenter,
-                  transform: Matrix4.skewY(0.0)..rotateZ(0.0),
-                  child: Container(
-                    padding: EdgeInsets.all(1.0),
-                    color: Color(0x77000000),
-                    width: 1400,
-                    child: new Text(
-                      "Flutter\nTutorial",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white
-                      ),
-                    ),
+                transform: Matrix4.skewY(0.0)..rotateZ(0.0),
+                child: Container(
+                  padding: EdgeInsets.all(1.0),
+                  color: Color(0x77000000),
+                  width: 1400,
+                  child: new Text(
+                    "Flutter\nTutorial",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
             ),
-            GestureDetector(
-              onDoubleTap: () => Navigator.pushNamed(context, "/firstPage"),
-              child: Container(
-                color: Colors.cyan,
-                margin: EdgeInsets.all(10.0),
-                alignment: Alignment.center,
-                child: Text(":)"),
+          ),
+          GestureDetector(
+            onDoubleTap: () => Navigator.pushNamed(context, "/firstPage"),
+            child: Container(
+              color: Colors.cyan,
+              margin: EdgeInsets.all(10.0),
+              alignment: Alignment.center,
+              child: Image.asset(
+                "assets/backgrounds/bg1.jpg",
               ),
             ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: FadeInImage.assetNetwork(
+              image: "https://scontent-frt3-2.cdninstagram.com/vp/1fa729d72a7f671b79166aa2c4343ee2/5C8D12DA/t51.2885-15/fr/e15/s1080x1080/45835041_619192898498236_5011079989747444985_n.jpg",
+              placeholder: "assets/gifs/loading.gif",
             ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-            new Container(
-              color: Colors.cyan,
-              margin: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text(":)"),
-            ),
-          ],
-        )
-      ),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+          new Container(
+            color: Colors.cyan,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(":)"),
+          ),
+        ],
+      )),
       bottomNavigationBar: new BottomNavigationBar(
         items: [
           new BottomNavigationBarItem(
             icon: new Icon(Icons.settings),
-            title: new Text('1.ci', textDirection: TextDirection.ltr,),
+            title: new Text(
+              '1.ci',
+              textDirection: TextDirection.ltr,
+            ),
           ),
           new BottomNavigationBarItem(
             icon: new Icon(Icons.settings),
-            title: new Text('1.ci', textDirection: TextDirection.ltr,),
+            title: new Text(
+              '1.ci',
+              textDirection: TextDirection.ltr,
+            ),
           ),
         ],
         onTap: (int i) {
@@ -195,15 +203,13 @@ class ScaffoldExample extends StatelessWidget {
       resizeToAvoidBottomPadding: true,
       primary: true,
       drawer: new Drawer(
-        child: new ListTile(
-          leading: Icon(Icons.settings),
-          title: Text('Hele Hele'),
-          onTap: () {
-            print('drawer\'a tiklandi');
-            Navigator.pop(context);
-          }
-        )
-      ),
+          child: new ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Hele Hele'),
+              onTap: () {
+                print('drawer\'a tiklandi');
+                Navigator.pop(context);
+              })),
     );
   }
 }
